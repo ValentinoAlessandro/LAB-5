854df128e3deca2064e3f331f2688b34698aad3d
ValentinoAlessandro <108438768+ValentinoAlessandro@users.noreply.github.com>
2022-07-15 19:26:16 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+
+import './App.css';
+import React,{useState} from 'react';
+function App() {
+// 1) Reemplaza estas variables por useState, puedes utilizar los valores iniciales que ya traen las variables
+
+let [temperatureValue,setTemperatureValue]=useState(10);
+let [temperatureColor,setTemperatureColor]=useState('cold');
+
+
+// 2) Agregar las funciones para manejar los eventos de Click a los botones para que 
+//     cuando se presionen se incremente o decremente el valor de la temperatura. 
+
+function Suma(){
+  setColor();
+  if(temperatureValue<30){
+  setTemperatureValue(temperatureValue+1)
+  }}
+  function Resta(){
+    setColor();
+    if(temperatureValue>0){
+    setTemperatureValue(temperatureValue-1)
+    }}
+// 3) Agrega una lógica para que al momento de que el valor sea >= 20 grados, la variable 'temperatureColor' cambie a 'hot'
+//    De igual manera si la temperatura baja a <20 grados, la variable cambie a 'cold'
+
+function setColor(){
+if(temperatureValue >=19 && temperatureValue<=30){
+  setTemperatureColor('hot');
+}else  {
+  setTemperatureColor('cold');
+}}
+// Para fines prácticos, agrega una regla que evite que los valores suban arriba de 30. Es decir al llegar a 30, no se podrá incrementar más.
+// De igual manera al llegar los valores a 0 no se podrá decrementas más.
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div  className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}°C
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={Suma} >+</button>
+        <button onClick={Resta} >-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
